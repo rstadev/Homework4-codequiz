7882c96cc256404dfbd610a40a1dcbb0a423fcf3
unknown <nukasine@gmail.com>
2021-02-20 18:06:52 -0500

    added an if statement that will prevent the interval from continously decreasing from successive clicks

--- script.js
+++ script.js
@@ -3,26 +3,30 @@ const quizContainer = document.getElementById('quiz-container');
 let secondsLeft = 100
 let timer = document.getElementById('timer')
 let button = document.getElementById("buttonMain");
+let timerInterval = 0
 
 // let buttonMain = document.getElementById('buttonMain')
 // buttonMain.textContent('Start!')
 button.addEventListener('click', function () {
   // console.log(button)
   function setTime() {
-      var timerInterval = setInterval(function() {
+    if (timerInterval === 0) {
+      timerInterval = setInterval(function() {
         secondsLeft--;
         // timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
         timer.textContent = "Time left: " + secondsLeft;
         timer.classList.add('text-center');
+      
 
     
-        if(secondsLeft === 0) {
+      if(secondsLeft === 0) {
           clearInterval(timerInterval);
           // sendMessage();
         }
     
       }, 1000);
     }
+  }
     setTime();
 
 })
