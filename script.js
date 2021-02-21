03c38b6e961c9deb509f7cdd36392b7070cb314e
unknown <nukasine@gmail.com>
2021-02-21 14:08:41 -0500

    added a method to disappear the welcome text once the button was pressed. Also added a questions object with two temp questions. Also added a seperate highscores.html file.

--- script.js
+++ script.js
@@ -2,7 +2,23 @@
 const quizContainer = document.getElementById('quiz-container');
 let secondsLeft = 100
 let timer = document.getElementById('timer')
-let button = document.getElementById("buttonMain");
+let button = document.getElementById('buttonMain');
+let index = 0;
+let welcomeText = document.getElementById('welcome');
+const questions = [
+  {questionTitle : 'What is the 2+2?',
+  choices : ['1', '2', '3', '4'],
+  answer:'4'
+  },
+  {questionTitle : 'Sample Question',
+  choices : ['wrong', 'wrong2', 'wrong3', 'correct'],
+  answer: 'correct'
+
+  }
+];
+
+
+
 
 // let buttonMain = document.getElementById('buttonMain')
 // buttonMain.textContent('Start!')
@@ -12,7 +28,7 @@ button.addEventListener('click', function () {
       var timerInterval = setInterval(function() {
         secondsLeft--;
         // timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
-        timer.textContent = "Time left: " + secondsLeft;
+        timer.textContent = 'Time left: ' + secondsLeft;
         timer.classList.add('text-center');
 
     
@@ -24,5 +40,12 @@ button.addEventListener('click', function () {
       }, 1000);
     }
     setTime();
+    welcomeText.innerHTML = ''
 
-})
+    // switchDiv();
+
+});
+
+// function switchDiv (index) {
+//   welcomeText.innerHTML = ''
+// }
