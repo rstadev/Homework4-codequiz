9aa5f006a4763bef78a1a5228922c9fe4ff65841
unknown <nukasine@gmail.com>
2021-02-22 15:49:07 -0500

    got buttons generated to have a click event, and a function now compares the answer picked to the correct answer and console logs it.

--- script.js
+++ script.js
@@ -5,26 +5,52 @@ let timer = document.getElementById('timer')
 const button = document.getElementById('buttonMain');
 let index = 0;
 let timerInterval = 0;
-let welcomeText = document.getElementById('welcome');
-let questionsText = document.getElementById('questions-container');
-let questionsList = document.getElementById('questionsList');
-console.log(questionsText);
-console.log(questionsList);
+const welcomeText = document.getElementById('welcome');
+const questionsText = document.getElementById('questions-container');
+const questionsList = document.getElementById('questionsList');
+  
 
+let score = 0;
+console.log(questionsText);
+// console.log(questionsList);
 
 
 const questions = [
   {
-  questionTitle : 'What is the 2+2?',
-  questionChoices : ['1', '2', '3', '4'],
-  questionAnswer:'4'
+    questionTitle : 'Sample Question',
+    questionChoices : ['wrong', 'wrong2', 'wrong3', 'correct'],
+    questionAnswer: 'correct',
+  
   },
-  {questionTitle : 'Sample Question',
+  {
+  questionTitle : 'Sample Question',
   questionChoices : ['wrong', 'wrong2', 'wrong3', 'correct'],
-  questionAnswer: 'correct'
+  questionAnswer: 'correct',
 
-  }
+  },
+  {
+    questionTitle : 'Sample Question',
+    questionChoices : ['wrong', 'wrong2', 'wrong3', 'correct'],
+    questionAnswer: 'correct',
+  
+  },
+  {
+    questionTitle : 'Sample Question',
+    questionChoices : ['wrong', 'wrong2', 'wrong3', 'correct'],
+    questionAnswer: 'correct',
+  
+  },
+  {
+    questionTitle : 'Sample Question',
+    questionChoices : ['wrong', 'wrong2', 'wrong3', 'correct'],
+    questionAnswer: 'correct',
+  
+  },
 ];
+// let generatedQuestion = questions[index].questionTitle;
+// let choicesArray = questions[index].questionChoices;
+
+
 
 
 
@@ -40,30 +66,74 @@ button.addEventListener('click', function () {
         // timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
         timer.textContent = 'Time left: ' + secondsLeft;
         timer.classList.add('text-center');
-      
-
-    
-      if(secondsLeft === 0) {
+        
+        
+        
+        if(secondsLeft === 0) {
           clearInterval(timerInterval);
           // sendMessage();
         }
-    
+        
       }, 1000);
     }
   }
-    setTime();
-    welcomeText.innerHTML = ''
-
-    // switchDiv();
-
+  setTime();
+  welcomeText.innerHTML = '';
+  
+  switchDiv(0);
+  
 });
 
-// function switchDiv (index) {
-//   // questionsText.innerHTML = ''
-//   for (let i = 0; i < questions.length; i++ ) {
-//     // let generatedChoices = questions[index].questionChoices
-//     let generatedQuestion = questions[index].questionTitle
-//     questionText.textContent = generatedQuestion
-//   }
+console.log(questions[index].questionTitle);
 
-// }
+function switchDiv (index) {
+    questionsText.innerHTML = '';
+    let generatedQuestion = questions[index].questionTitle;
+    let choicesArray = questions[index].questionChoices;
+  for (let i = 0; i < questions.length; i++ ) {
+    questionsText.textContent = generatedQuestion;
+  };
+
+  choicesArray.forEach(function (newItemGen) {
+    let itemList = document.createElement('button');
+    itemList.textContent = newItemGen;
+    itemList.setAttribute('class', 'choiceButtons');
+    itemList.setAttribute('onclick', 'checkAnswer(this.textContent)');
+    console.log(newItemGen)
+    questionsText.appendChild(itemList);
+    
+  })
+  // itemList.addEventListener('click', checkAnswer());
+};
+
+let clickButtons = document.querySelector('.choiceButtons');
+console.log(clickButtons)
+
+
+
+
+// document.querySelector('choiceButtons').onclick = function () {
+//   console.log('Click!')
+
+  function checkAnswer(val) {
+    console.log(val);
+    console.log(questions[index].questionAnswer);
+    if (val.textContent === questions[index].questionAnswer) {
+     console.log('correct!') 
+    }
+  };
+
+
+
+
+//---------------------Future code that will likely help with Highscores------------
+// const arrParsed = JSON.parse(localStorage.getItem('name')) || []
+
+// const addToStoraged = item => {
+//   arrParsed.push(item)
+//     localStorage.setItem('name', JSON.stringify(arrParsed))
+//   };
+// addToStoraged();
+// console.log('arrParsed==>>', arrParsed);
+//       //document.getElementById('app').textContent = name
+//--------------------------------------------------------------------------------------
